725c4e7feb083ecbfff4180c32d7a26239e0512b
szyrelleg-tmQ <tmq.szy.g@gmail.com>
2025-07-15 13:12:04 -0400

    feat(audio-manager): add participant count update on initialization and streamline count retrieval during translation message processing

--- bigbluebutton-html5/imports/ui/services/audio-manager/index.js
+++ bigbluebutton-html5/imports/ui/services/audio-manager/index.js
@@ -765,11 +765,18 @@ class AudioManager {
             videoSource: false,
           },
         });
+        const updateParticipantsCount = () => {
+          const participants = callObject.participants();
+          this.participantsCount = Object.keys(participants).length;
+          console.log('[TRANSLATOR] Participants count updated:=========================', this.participantsCount);
+        };
+        updateParticipantsCount();
+
         console.log('[TRANSLATOR] Initializing translator call object with language:', language, 'and voice:', voice, 'for room:', roomId);
 
         this._translatorCallObject = callObject;
         const data = await callObject.startBot(currentName, language, roomId, voice);
-        console.log(data)
+
         callObject.on('participant-joined', (event) => {
           console.log('✅ Successfully joined the Daily room!', event);
           dailyCoIntegration.initialize(callObject);
@@ -816,11 +823,6 @@ class AudioManager {
             translationBuffer[messageId].count += 1;
             // When all expected translations are received, send compressed message
             if (translationBuffer[messageId].count === this.participantsCount) {
-              const updateParticipantsCount = () => {
-                const participants = callObject.participants();
-                this.participantsCount = Object.keys(participants).length;
-              };
-              updateParticipantsCount();
               const messageObj = {
                 timestamp: messageId,
                 original: data.text,
