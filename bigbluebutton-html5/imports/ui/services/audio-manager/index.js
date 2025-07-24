78e682cf52e7a1624e5c95cdf07e497ba8194fe1
szyrelleg-tmQ <tmq.szy.g@gmail.com>
2025-07-24 12:22:26 -0400

    refactor(audio-manager): simplify key generation for translation buffers by removing participant text dependency

--- bigbluebutton-html5/imports/ui/services/audio-manager/index.js
+++ bigbluebutton-html5/imports/ui/services/audio-manager/index.js
@@ -879,7 +879,7 @@ class AudioManager {
               type: data.type,
             });
 
-            const key = `${data.text}|${data.participant_name}`;
+            const key = data.participant_name;
             if (!translationBuffers[key]) {
               translationBuffers[key] = {
                 original: data.text,
