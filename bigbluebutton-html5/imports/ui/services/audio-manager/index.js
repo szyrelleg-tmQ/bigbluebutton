273afa5d2c7b696462bf2e5af7122635a977ad0c
szyrelleg-tmQ <tmq.szy.g@gmail.com>
2025-07-30 09:33:47 -0400

    refactor(audio-manager): comment out unused event handling logic for bot and user speaking to streamline code

--- bigbluebutton-html5/imports/ui/services/audio-manager/index.js
+++ bigbluebutton-html5/imports/ui/services/audio-manager/index.js
@@ -954,58 +954,58 @@ class AudioManager {
 
         dailyManager.on("app-message", (message) => {
           const data = message.data;
-          if (data.event_type === "bot_started_speaking") {
-            if (data && data.id && data.id != this.localBot) {
-              EventManager.emit(EVENTS.LOCAL_TRANLATED_START, data);
-              console.log('[TRANSLATOR EVENTS] Local bot started speaking:', data);
-            } else {
-              EventManager.emit(EVENTS.REMOTE_TRANLATED_START, data);
-              console.log('[TRANSLATOR EVENTS] Remote bot started speaking:', data);
-            }
-          }
-          if (data.event_type === "bot_stopped_speaking") {
-            if (data && data.id && data.id != this.localBot) {
-              EventManager.emit(EVENTS.LOCAL_TRANLATED_END, data);
-              console.log('[TRANSLATOR EVENTS] Local bot stopped speaking:', data);
-            } else {
-              EventManager.emit(EVENTS.REMOTE_TRANLATED_END, data);
-              console.log('[TRANSLATOR EVENTS] Remote bot stopped speaking:', data);
-            }
-          }
-          if (data.event_type === "user_started_speaking") {
-            if (data && data.id && !data.id.includes(currentName)) {
-              EventManager.emit(EVENTS.LOCAL_RAW_START, data);
-              console.log('[TRANSLATOR EVENTS] Local user started speaking:', data.id, currentName, "-==========");
-            } else {
-              EventManager.emit(EVENTS.REMOTE_RAW_START, data);
-              console.log('[TRANSLATOR EVENTS] Remote user started speaking:', data.id, currentName, "-==========");
-            }
-          }
-          if (data.event_type === "user_stopped_speaking") {
-            if (data && data.id && !data.id.includes(currentName)) {
-              console.log('[TRANSLATOR EVENTS] Local user stopped speaking:', data);
-              EventManager.emit(EVENTS.LOCAL_RAW_END, data);
-            } else {
-              console.log('[TRANSLATOR EVENTS] Remote user stopped speaking:', data);
-              EventManager.emit(EVENTS.REMOTE_RAW_END, data);
-            }
-          }
-          if (data.event_type === 'language_detected') {
-            console.log('[TRANSLATOR] Language detected:', data);
-          }
-          if (data.insufficient_data) {
-            console.warn('[TRANSLATOR] Insufficient data for language detection:', data);
-          }
-          if (data.confidence < 0.7) {
-            console.warn('[TRANSLATOR] Low confidence in language detection:', data);
-          }
-          if (data.service_unavailable) {
-            console.error('[TRANSLATOR] Language detection service unavailable:', data);
-          }
-          if (data.event_type === "p2p_voice_change_request") {
-            const { data, fromId } = message;
-            dailyManager.handleP2PMessage(data, fromId);
-          }
+          // if (data.event_type === "bot_started_speaking") {
+          //   if (data && data.id && data.id != this.localBot) {
+          //     EventManager.emit(EVENTS.LOCAL_TRANLATED_START, data);
+          //     console.log('[TRANSLATOR EVENTS] Local bot started speaking:', data);
+          //   } else {
+          //     EventManager.emit(EVENTS.REMOTE_TRANLATED_START, data);
+          //     console.log('[TRANSLATOR EVENTS] Remote bot started speaking:', data);
+          //   }
+          // }
+          // if (data.event_type === "bot_stopped_speaking") {
+          //   if (data && data.id && data.id != this.localBot) {
+          //     EventManager.emit(EVENTS.LOCAL_TRANLATED_END, data);
+          //     console.log('[TRANSLATOR EVENTS] Local bot stopped speaking:', data);
+          //   } else {
+          //     EventManager.emit(EVENTS.REMOTE_TRANLATED_END, data);
+          //     console.log('[TRANSLATOR EVENTS] Remote bot stopped speaking:', data);
+          //   }
+          // }
+          // if (data.event_type === "user_started_speaking") {
+          //   if (data && data.id && !data.id.includes(currentName)) {
+          //     EventManager.emit(EVENTS.LOCAL_RAW_START, data);
+          //     console.log('[TRANSLATOR EVENTS] Local user started speaking:', data.id, currentName, "-==========");
+          //   } else {
+          //     EventManager.emit(EVENTS.REMOTE_RAW_START, data);
+          //     console.log('[TRANSLATOR EVENTS] Remote user started speaking:', data.id, currentName, "-==========");
+          //   }
+          // }
+          // if (data.event_type === "user_stopped_speaking") {
+          //   if (data && data.id && !data.id.includes(currentName)) {
+          //     console.log('[TRANSLATOR EVENTS] Local user stopped speaking:', data);
+          //     EventManager.emit(EVENTS.LOCAL_RAW_END, data);
+          //   } else {
+          //     console.log('[TRANSLATOR EVENTS] Remote user stopped speaking:', data);
+          //     EventManager.emit(EVENTS.REMOTE_RAW_END, data);
+          //   }
+          // }
+          // if (data.event_type === 'language_detected') {
+          //   console.log('[TRANSLATOR] Language detected:', data);
+          // }
+          // if (data.insufficient_data) {
+          //   console.warn('[TRANSLATOR] Insufficient data for language detection:', data);
+          // }
+          // if (data.confidence < 0.7) {
+          //   console.warn('[TRANSLATOR] Low confidence in language detection:', data);
+          // }
+          // if (data.service_unavailable) {
+          //   console.error('[TRANSLATOR] Language detection service unavailable:', data);
+          // }
+          // if (data.event_type === "p2p_voice_change_request") {
+          //   const { data, fromId } = message;
+          //   dailyManager.handleP2PMessage(data, fromId);
+          // }
           if (data.event_type === "transcription") {
             latestTranscriptionVar({
               text: data.text,
