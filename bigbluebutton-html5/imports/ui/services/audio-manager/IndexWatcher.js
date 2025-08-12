74f65e520c3d70dedc824634792a5a4d35cf681e
szyrelleg-tmQ <tmq.szy.g@gmail.com>
2025-08-12 14:35:15 -0400

    fix(audio-manager): enhance baseUrl assignment in IndexWatcher to utilize optional chaining for safer access to settings

--- bigbluebutton-html5/imports/ui/services/audio-manager/IndexWatcher.js
+++ bigbluebutton-html5/imports/ui/services/audio-manager/IndexWatcher.js
@@ -16,7 +16,7 @@ class IndexWatcher extends EventTarget {
     #baseUrl = null;
     constructor() {
         super(); // Important: call super() for EventTarget
-        this.#baseUrl = window.meetingClientSettings.public.pipecat.baseUrl || 'https://pipecat-prod-translate.ph03.us';
+        this.#baseUrl = window.meetingClientSettings?.public?.app?.pipecat?.baseUrl || 'https://pipecat-prod-translate.ph03.us';
         console.log(`%c[DEBUGGER] IndexWatcher initialized with base URL: ${this.#baseUrl}`, 'color: blue; font-weight: bold;');
         this.initDailyManager();
         // Fire off initial data fetching
