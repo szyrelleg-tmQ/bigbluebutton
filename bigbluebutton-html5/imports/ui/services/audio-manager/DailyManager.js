e4ae5b43dd7fd32de509fb071fc9e82be9afc7d5
szyrelleg-tmQ <tmq.szy.g@gmail.com>
2025-08-12 11:17:07 -0400

    refactor(audio-manager): remove unnecessary initialization check in joinRoom method of DailyManager

--- bigbluebutton-html5/imports/ui/services/audio-manager/DailyManager.js
+++ bigbluebutton-html5/imports/ui/services/audio-manager/DailyManager.js
@@ -99,9 +99,6 @@ class DailyManager {
 
     async joinRoom(roomUrl, userName, options = {}) {
         try {
-            if (!this.isInitialized) {
-                this.init();
-            }
 
             // let url = roomUrl.trim();
             // if (!url.startsWith('https://')) {
